68a96657f8411abe2abb95ccd6d4778d1b7ebd56
id777 <id777.doni@gmail.com>
2025-05-06 17:32:08 -0500

    updated Hooks.java

--- src/test/java/DanceGroupPractice/com/step_definitions/Hooks.java
+++ src/test/java/DanceGroupPractice/com/step_definitions/Hooks.java
@@ -1,6 +1,7 @@
 package DanceGroupPractice.com.step_definitions;
 
 import DanceGroupPractice.com.utilities.Driver;
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
 import java.time.Duration;
@@ -8,6 +9,13 @@ import java.time.Duration;
 public class Hooks {
     @Before
     public void setup() {
+        Driver.getDriver().manage().window().maximize();
         Driver.getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
+
+    @After
+    public void teardown(){
+        Driver.closeDriver();
+    }
+
 }
